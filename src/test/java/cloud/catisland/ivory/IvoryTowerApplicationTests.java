af392289a4f25fe53d1b944215a9f561fde604a3
Xy718 <869839000@qq.com>
2020-05-21 16:25:51 +0800

    init

---
+++ src/test/java/cloud/catisland/ivory/IvoryTowerApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.catisland.ivory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IvoryTowerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
