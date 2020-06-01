8558e114f5497d2f7184678df4647c056e1a1d74
Xy718 <869839000@qq.com>
2020-06-01 15:32:54 +0800

    123

--- src/main/java/cloud/catisland/ivory/system/controller/LoginController.java
+++ src/main/java/cloud/catisland/ivory/system/controller/LoginController.java
@@ -8,6 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 import cloud.catisland.ivory.system.model.BO.RegBO;
 import cloud.catisland.ivory.system.model.BO.ResultBean;
 
+/**
+ * @Author: Xy718
+ * @Date: 2020-05-25 23:15:14
+ * @LastEditors: Xy718
+ * @LastEditTime: 2020-06-01 01:13:39
+ */
 @RestController
 @RequestMapping("/login")
 public class LoginController {
