42b268e8143b9c977e1725130653316a9920a38e
Xy718 <869839000@qq.com>
2020-06-24 23:31:23 +0800

    asdasd

--- startup.sh
+++ startup.sh
@@ -1,18 +1,5 @@
 #!/usr/bin/env bash
 jar_name=./build/libs/ivory.jar
-cmd=$"java -jar -Xmx500M ${jar_name}\r";
-screen_name=ivory
 
-# #发送命令，结束当前screen运行的jar
-# screen -r -d ${screen_name} && $'\003' #ctrl+c
-# #发送命令，结束当前screen
-# screen -X -S ${screen_name} quit
-# #新建screen
-# # screen -S ${screen_name} && ""
-# screen -S $screen_name -p 0 -X stuff "ls^M"
-# screen -S ivory
+cd /ci_file/icory_backend
 nohup java -jar -Xmx500M ${jar_name} &
-echo "screen:${screen_name} 创建完毕"
-
-# screen -x -S $screen_name -X stuff "$cmd"
-# screen -x -S $screen_name -X stuff $'\n'
