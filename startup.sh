522388c07a5c876ae94d0257c955ceb3870763b9
Xy718 <869839000@qq.com>
2020-06-24 17:41:15 +0800

    测试一下

--- startup.sh
+++ startup.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 jar_name=./build/libs/ivory.jar
+screen_name=ivory
 #发送命令，结束当前screen运行的jar
-screen -r -d ivory stuff $'\003' #ctrl+c
+screen -r -d ${screen_name} && $'\003' #ctrl+c
 #发送命令，结束当前screen
-screen -X -S ivory quit
+screen -X -S ${screen_name} quit
 #新建screen
-screen -S ivory
+screen -S ${screen_name}
 #发送命令，启动jar
-screen -r -d ivory stuff "java -jar -Xmx500M ${jar_name}\r"
+screen -r -d ${screen_name} && "java -jar -Xmx500M ${jar_name}\r"
