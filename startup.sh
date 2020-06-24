c6a9ddaac530bd1eb6028b105434d15675327894
Xy718 <869839000@qq.com>
2020-06-24 18:02:57 +0800

    撒大声地啥多啥多

--- startup.sh
+++ startup.sh
@@ -2,13 +2,14 @@
 jar_name=./build/libs/ivory.jar
 cmd=$"java -jar -Xmx500M ${jar_name}\r";
 screen_name=ivory
+
 #发送命令，结束当前screen运行的jar
 screen -r -d ${screen_name} && $'\003' #ctrl+c
 #发送命令，结束当前screen
 screen -X -S ${screen_name} quit
 #新建screen
 # screen -S ${screen_name} && ""
-screen -x -S $screen_name -X stuff "$cmd"
+screen -S $screen_name -X stuff "$cmd"
 echo "screen:${screen_name} 创建完毕"
 
 # screen -x -S $screen_name -X stuff "$cmd"
