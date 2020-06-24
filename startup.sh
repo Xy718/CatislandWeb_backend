d031c708b81628a98fe932cdd72715fa8c4ba07a
Xy718 <869839000@qq.com>
2020-06-24 17:35:17 +0800

    00000

--- startup.sh
+++ startup.sh
@@ -3,8 +3,8 @@ jar_name=./build/libs/ivory.jar
 #发送命令，结束当前screen运行的jar
 screen -r -d ivory stuff $'\003' #ctrl+c
 #发送命令，结束当前screen
-screen -S ivory -X stuff 'exit\r'
+screen -X -S ivory quit
 #新建screen
-screen -dmS ivory
+screen -S ivory
 #发送命令，启动jar
 screen -r -d ivory stuff "java -jar -Xmx500M ${jar_name}\r"
