ad72add6b2e901a364c964d40e50126cb9a228e1
Xy718 <869839000@qq.com>
2020-06-28 17:27:45 +0800

    已完成，宝贝

--- startup.sh
+++ startup.sh
@@ -8,7 +8,10 @@ port=11718
 pid=$(netstat -nlp | grep :$port | awk '{print $7}' | awk -F"/" '{ print $1 }');
 #如果存在，kill进程
 if [ -n "$pid" ]; then 
-    kill -9 $pid; 
+    kill -9 $pid;
+    echo "删除了被占用的端口的进程";
+else
+    echo "端口未被占用";
 fi
 
 nohup java -jar -Xmx500M ${jar_name} &
