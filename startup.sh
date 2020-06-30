d6a80cdd17e096800be6eec5327ec73dd814ba18
Xy718 <869839000@qq.com>
2020-06-30 23:44:26 +0800

    启动脚本中增加了5s的停顿

--- startup.sh
+++ startup.sh
@@ -3,18 +3,21 @@ jar_name=./build/libs/ivory.jar
 
 cd ci_file/ivory_backend
 
-#找到已在运行或占用端口的进程
-# port=11718
-# pid=$(netstat -nlp | grep :$port | awk '{print $7}' | awk -F"/" '{ print $1 }');
-# #如果存在，kill进程
-# if [ -n "$pid" ]; then 
-#     kill -9 $pid;
-#     echo "删除了被占用的端口的进程";
-# else
-#     echo "端口未被占用";
-# fi
 #停止已在运行的api
 screen -S ivory -X stuff '^C\n'
+echo "等待进程停止"
+sleep 10s
+#检测端口是否有占用
+port=11718
+pid=$(netstat -nlp | grep :$port | awk '{print $7}' | awk -F"/" '{ print $1 }');
+#如果存在，kill进程
+if [ -n "$pid" ]; then 
+    kill -9 $pid;
+    echo "删除了被占用的端口的进程";
+else
+    echo "端口未被占用";
+fi
+
 screen -X -S ivory quit
 screen -dmS ivory
 screen -x -S ivory -X stuff "java -jar -Xmx500M ${jar_name} \n"
