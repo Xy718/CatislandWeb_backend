25de9f0e069610fc88eec93e8c1e8185237c9bfd
Xy718 <869839000@qq.com>
2020-07-20 00:14:06 +0800

    1

--- src/main/java/cloud/catisland/ivory/common/dao/repository/TopicRepository.java
+++ src/main/java/cloud/catisland/ivory/common/dao/repository/TopicRepository.java
@@ -3,7 +3,6 @@ package cloud.catisland.ivory.common.dao.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import cloud.catisland.ivory.common.dao.model.Topic;
 /**
